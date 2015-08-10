c786aa2535613b33dbbbe2d79ffee25bfc77292a
tonussi <lptonussi@gmail.com>
2015-08-10 12:04:40 -0300

    Add resp exerc2 19 1a

--- mips/exercicio2-19-1a-patterson.asm
+++ mips/exercicio2-19-1a-patterson.asm
@@ -1,14 +1,27 @@
+# Exercise 2.19
+#   For the following problems, the table holds C code functions.
+#   Assume that the first function listed in the table is called
+#   first. You will be asked to translate these C code routines
+#   into MIPS assembly.
+
+# int fib(int n) {
+#   if (n==0)
+#     return 0;
+#   else if (n == 1)
+#     return 1;
+#   else
+#     fib(n-1) + fib(n–2);
+
+# 2.19.1 [15] <2.8> Implement the C code in the table
+# in MIPS assembly. What is the total number of MIPS
+# instructions needed to execute the function?
+
 .text
 .globl main
 
 main:
 
-  #  int fib (int n)
-  #    if (n == 0) return 0;
-  #    elif (n == 1) return 1;
-  #    else return fib(n - 1) + fib(n - 2);
-
-  addi $a0, $zero, 8
+  addi $a0, $zero, 5
   jal fib
 
   # c..
@@ -20,6 +33,8 @@ fib:
 	addi $sp, $sp, -8
 	sw $ra, 4($sp)
 	sw $a0, 0($sp)
+
+	beq $0, $a0, exit
 
   slti $t0, $a0, 1
   beq $t0, $zero, L1
@@ -51,3 +66,5 @@ L1:
   jr $ra
 
 exit:
+
+	addi $v0, $0, 0
