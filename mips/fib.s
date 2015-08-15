7bc77ef7969e30e623b36618b93eec84dae23b08
tonussi <lptonussi@gmail.com>
2015-08-15 18:41:25 -0300

    Add fib em mips

--- mips/fib.s
+++ mips/fib.s
@@ -1,103 +1,72 @@
-	.file	"fib.c"
-# GNU C (Ubuntu/Linaro 4.8.1-10ubuntu9) version 4.8.1 (x86_64-linux-gnu)
-#	compiled by GNU C version 4.8.1, GMP version 5.1.2, MPFR version 3.1.1-p2, MPC version 1.0.1
-# GGC heuristics: --param ggc-min-expand=100 --param ggc-min-heapsize=131072
-# options passed:  -imultiarch x86_64-linux-gnu fib.c -mtune=generic
-# -march=x86-64 -auxbase-strip fib.s -fverbose-asm -fstack-protector
-# -Wformat -Wformat-security
-# options enabled:  -faggressive-loop-optimizations
-# -fasynchronous-unwind-tables -fauto-inc-dec -fbranch-count-reg -fcommon
-# -fdelete-null-pointer-checks -fdwarf2-cfi-asm -fearly-inlining
-# -feliminate-unused-debug-types -ffunction-cse -fgcse-lm -fgnu-runtime
-# -fident -finline-atomics -fira-hoist-pressure -fira-share-save-slots
-# -fira-share-spill-slots -fivopts -fkeep-static-consts
-# -fleading-underscore -fmath-errno -fmerge-debug-strings
-# -fmove-loop-invariants -fpeephole -fprefetch-loop-arrays
-# -freg-struct-return -fsched-critical-path-heuristic
-# -fsched-dep-count-heuristic -fsched-group-heuristic -fsched-interblock
-# -fsched-last-insn-heuristic -fsched-rank-heuristic -fsched-spec
-# -fsched-spec-insn-heuristic -fsched-stalled-insns-dep -fshow-column
-# -fsigned-zeros -fsplit-ivs-in-unroller -fstack-protector
-# -fstrict-volatile-bitfields -fsync-libcalls -ftrapping-math
-# -ftree-coalesce-vars -ftree-cselim -ftree-forwprop -ftree-loop-if-convert
-# -ftree-loop-im -ftree-loop-ivcanon -ftree-loop-optimize
-# -ftree-parallelize-loops= -ftree-phiprop -ftree-pta -ftree-reassoc
-# -ftree-scev-cprop -ftree-slp-vectorize -ftree-vect-loop-version
-# -funit-at-a-time -funwind-tables -fverbose-asm -fzero-initialized-in-bss
-# -m128bit-long-double -m64 -m80387 -maccumulate-outgoing-args
-# -malign-stringops -mfancy-math-387 -mfp-ret-in-387 -mfxsr -mglibc
-# -mieee-fp -mlong-double-80 -mmmx -mno-sse4 -mpush-args -mred-zone -msse
-# -msse2 -mtls-direct-seg-refs
-
-	.section	.rodata
-.LC0:
-	.string	"%d"
+	.file	1 "fib.c"
+	.section .mdebug.abi32
+	.previous
+	.gnu_attribute 4, 1
 	.text
-	.globl	main
-	.type	main, @function
-main:
-.LFB0:
-	.cfi_startproc
-	pushq	%rbp	#
-	.cfi_def_cfa_offset 16
-	.cfi_offset 6, -16
-	movq	%rsp, %rbp	#,
-	.cfi_def_cfa_register 6
-	movl	$5, %edi	#,
-	movl	$0, %eax	#,
-	call	fib	#
-	movl	%eax, %esi	# D.2195,
-	movl	$.LC0, %edi	#,
-	movl	$0, %eax	#,
-	call	printf	#
-	popq	%rbp	#
-	.cfi_def_cfa 7, 8
-	ret
-	.cfi_endproc
-.LFE0:
-	.size	main, .-main
+	.align	2
 	.globl	fib
+	.set	nomips16
+	.ent	fib
 	.type	fib, @function
 fib:
-.LFB1:
-	.cfi_startproc
-	pushq	%rbp	#
-	.cfi_def_cfa_offset 16
-	.cfi_offset 6, -16
-	movq	%rsp, %rbp	#,
-	.cfi_def_cfa_register 6
-	pushq	%rbx	#
-	subq	$24, %rsp	#,
-	.cfi_offset 3, -24
-	movl	%edi, -20(%rbp)	# n, n
-	cmpl	$0, -20(%rbp)	#, n
-	jne	.L3	#,
-	movl	$0, %eax	#, D.2197
-	jmp	.L4	#
-.L3:
-	cmpl	$1, -20(%rbp)	#, n
-	jne	.L5	#,
-	movl	$1, %eax	#, D.2197
-	jmp	.L4	#
-.L5:
-	movl	-20(%rbp), %eax	# n, tmp65
-	subl	$1, %eax	#, D.2197
-	movl	%eax, %edi	# D.2197,
-	call	fib	#
-	movl	%eax, %ebx	#, D.2197
-	movl	-20(%rbp), %eax	# n, tmp66
-	subl	$2, %eax	#, D.2197
-	movl	%eax, %edi	# D.2197,
-	call	fib	#
-	addl	%ebx, %eax	# D.2197, D.2197
-.L4:
-	addq	$24, %rsp	#,
-	popq	%rbx	#
-	popq	%rbp	#
-	.cfi_def_cfa 7, 8
-	ret
-	.cfi_endproc
-.LFE1:
+	.frame	$fp,32,$31		# vars= 0, regs= 3/0, args= 16, gp= 0
+	.mask	0xc0010000,-4
+	.fmask	0x00000000,0
+	.set	noreorder
+	.set	nomacro
+	addiu	$sp,$sp,-32
+	sw	$31,28($sp)
+	sw	$fp,24($sp)
+	sw	$16,20($sp)
+	move	$fp,$sp
+	sw	$4,32($fp)
+	lw	$2,32($fp)
+	nop
+	bne	$2,$0,$L2
+	nop
+
+	move	$2,$0
+	j	$L3
+	nop
+
+$L2:
+	lw	$3,32($fp)
+	li	$2,1			# 0x1
+	bne	$3,$2,$L4
+	nop
+
+	li	$2,1			# 0x1
+	j	$L3
+	nop
+
+$L4:
+	lw	$2,32($fp)
+	nop
+	addiu	$2,$2,-1
+	move	$4,$2
+	jal	fib
+	nop
+
+	move	$16,$2
+	lw	$2,32($fp)
+	nop
+	addiu	$2,$2,-2
+	move	$4,$2
+	jal	fib
+	nop
+
+	addu	$2,$16,$2
+$L3:
+	move	$sp,$fp
+	lw	$31,28($sp)
+	lw	$fp,24($sp)
+	lw	$16,20($sp)
+	addiu	$sp,$sp,32
+	j	$31
+	nop
+
+	.set	macro
+	.set	reorder
+	.end	fib
 	.size	fib, .-fib
-	.ident	"GCC: (Ubuntu/Linaro 4.8.1-10ubuntu9) 4.8.1"
-	.section	.note.GNU-stack,"",@progbits
+	.ident	"GCC: (GNU) 4.8.2"
