7dbefd00b57b4af4e2e07ac06b67501f1ec5ae3c
tonussi <lptonussi@gmail.com>
2015-08-15 18:43:57 -0300

    Add comparacao entre lor e orbit

--- mips/lor.c
+++ mips/lor.c
@@ -1,7 +1,7 @@
-#include <stdio.h>
+logical_or(short a, short b) {
+  return a || b; // should return true or false, like 1 or 0.
+}
 
-main()
-{
-  printf("%i", 2 || 3);
-  printf("%i", 2 | 3);
+bitwise_or(char c, char d) {
+  return c | d; // should do like 'A' lor 'B' = 1010 or 1011 = 00...001011
 }
